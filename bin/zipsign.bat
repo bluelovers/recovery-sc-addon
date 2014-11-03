86debae001e1167a49c787a64f420781148d8a73
bluelovers <codelovers@users.sourceforge.net>
2014-11-03 16:20:00 +0800

    add zip sign (Step: 4)
    Step: 4

---
+++ bin/zipsign.bat
@@ -0,0 +1,11 @@
+
+@echo OFF
+
+echo Input		: %1.zip
+echo Output		: %1-signed.zip
+
+java -jar "%~dp0\signapk.jar" "%~dp0\testkey.x509.pem" "%~dp0\testkey.pk8" %1.zip %1-signed.zip
+
+echo done.
+ 
+pause
